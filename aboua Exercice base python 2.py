0fd0c14ef53f47dcdd301af2c2bf47b57287b869
abouakouame <rogerfransois@gmail.com>
2021-10-11 14:51:10 +0000

    first commit

---
+++ aboua Exercice base python 2.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#Exercice Python base 2
+
+# Exercice 1.1
+# une fonction qui retourne la factorielle d'un nombre entier N.
+N=int(input("saisir un nombre ")) 
+F=1
+for i in range(2,N+1) :
+    F*=i
+print("le factoriel de ",N," est ",F)
+
+
+# In[36]:
+
+
+
+
+
+# In[1]:
+
+
+#Exercice Python base 2
+
+# Exercice 1.3
+# Définir la liste : liste =[17, 38, 10, 25, 72], puis effectuez les actions suivantes :
+
+nombres = [17, 38, 10, 25, 72]
+print(" Liste initiale ".center(50, '-'))
+print(nombres, '\n')
+rien = input('"cliquez sur Entree pour faire le tri"')
+print("  Tri ".center(50, '-'))
+nombres.sort()
+print(nombres, '\n')
+rien = input('"cliquez sur Entree pour ajoute "')
+print(" Ajout le chiffre 12 ".center(50, '-'))
+nombres.append(12)
+print(nombres, '\n')
+rien = input('"cliquez sur Entree pour retourne"')
+print(" Retournement ".center(50, '-'))
+nombres.reverse()
+print(nombres, '\n')
+rien = input('"cliquez sur Entree pour indiquez"')
+print(" Indice d'un element ".center(50, '-'))
+print(nombres.index(17), '\n')
+rien = input('"cliquez sur Entree pour supprimer 38"')
+print(" Retrait d'un element ".center(50, '-'))
+nombres.remove(38)
+print(nombres, '\n')
+rien = input('"Entree"')
+print(" Indicage ".center(50, '-'))
+print("nombres[1:3] =", nombres[1:3])
+print("nombres[:2] =", nombres[:2])
+print("nombres[2:] =", nombres[2:])
+
+
+# In[35]:
+
+
+#Exercice Python base 2
+
+# Exercice 1.3
+#listes 
+liste = [1,1,2,5,8,47,9,6,2,1,8,8,8,9,1,8,8,8,1,1,1]
+liste_sans = []
+for i in liste:
+    if i not in liste_sans:
+        liste_sans.append(i)
+ 
+         
+liste_sans
+[1, 2, 5, 8, 47, 9, 6]
+
+
+# In[38]:
+
+
+
+
+
+# In[49]:
+
+
+#Exercice Python base 2
+
+# Exercice 1.2
+liste = [N]
+N=int(input("saisir un nombre ")) 
+impair = []
+a = 0
+b = len(liste)
+while a < b:
+	d = liste[a]
+	if d % 2:
+		impair.append(d)
+	else:
+		pair.append(d)
+	a = a + 1
+print ("De la liste donnée 32 5 12 8 3 75 2 15")
+print ("les chiffres impairs sont", impair)
+
+
+# In[ ]:
+
+
+
+
---
+++ aboua exercice python base 1.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+masse = float(input("Saisir votre poids (en kg) : "))
+taille = float(input("Saisir votre taille (en cm) : "))
+taille = taille/100
+IMC = masse/taille/taille
+print("Votre IMC est de ",round(IMC,2))
+print("Votre corpulence est classée dans la catégorie : ",end="")
+if IMC < 16.5:
+ print("anorexie")
+elif IMC < 18.5:
+ print("maigreur")
+elif IMC < 25:
+ print("poids normal")
+elif IMC < 30:
+ print("surpoids")
+elif IMC < 35:
+ print("obésité modérée")
+elif IMC < 40:
+ print("obésité sévère")
+else:
+ print("obésité morbide")
+
+
+# In[13]:
+
+
+note=float(input("saisir une note : ")) 
+if note<10 :
+    print("non admis") 
+elif note<=10.9 :
+    print("passable") 
+elif note<=13.5 :
+    print("assez bien") 
+elif note<=16 :
+    print("bien") 
+elif note<=18 :
+    print("tres bien") 
+else :
+    print("saisir une note valide")
+
+
+# In[ ]:
+
+
+from math import pi
+     
+def CircleArea(r):
+    return pi*(r*r)
+print ( "l'aire d'un cercle de rayon de 1 cm est "+str(CircleArea(1))+"cm2")
+print ("l'aire d'un cercle de rayon de 5 cm est "+str(CircleArea(5))+"cm2")
+print( "l'aire d'un cercle de rayon de 8 cm est "+str(CircleArea(8))+"cm2")
+print ("l'aire d'un cercle de rayon de 10 cm est "+str(CircleArea(10))+"cm2")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
